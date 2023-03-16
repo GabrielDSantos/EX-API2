4fc59f696e5490c5b0f45a193aeb8acc75d29d5b
Gabriel Santos <gabrielsantos022@gmail.com>
2023-03-16 17:16:16 -0300

    update

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,17 +1,6 @@
-/* eslint-disable class-methods-use-this */
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Gabriel',
-      sobrenome: 'Santos',
-      email: 'gabrielsantos022@gmail.com',
-      idade: 19,
-      peso: 72,
-      altura: 1.81,
-    });
-    res.json(novoAluno);
+    res.json('index');
   }
 }
 
---
+++ src/database/seeds/20230310150029-criar-usuarios.js
@@ -0,0 +1,33 @@
+const bcryptjs = require('bcryptjs');
+
+module.exports = {
+  up: async (queryInterface) => queryInterface.bulkInsert(
+    'users',
+    [
+      {
+        nome: 'Opzin',
+        email: 'usuariodeoplayer@email.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nome: 'RamonWiiBR',
+        email: 'wolong@email.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nome: 'Mao Zedong',
+        email: 'kawaiexpulso@email.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ],
+    {},
+  ),
+
+  down: () => {},
+};
